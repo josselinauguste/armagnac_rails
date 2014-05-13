ae9d4a555e85e2357dfaf6674385aa9e46e7eafb
Josselin Auguste <jauguste@iblop.net>
2014-05-13 22:46:28 +0200

    Fix mailer preview

--- test/mailers/previews/digest_mailer_preview.rb
+++ test/mailers/previews/digest_mailer_preview.rb
@@ -2,6 +2,8 @@
 class DigestMailerPreview < ActionMailer::Preview
 	def feed_digest
 		feed = Feed.first
+		feed.fetched_at = nil
+		feed.fetch
 		DigestMailer.feed_digest(feed.title, feed.new_entries, 'jauguste@iblop.net')
 	end
 end
--- test/models/feed_test.rb
+++ test/models/feed_test.rb
@@ -12,12 +12,21 @@ class FeedTest < ActiveSupport::TestCase
 
   test 'get new entries only if already fetched' do
     feed = feeds(:one)
-    feed.update_attribute(:fetched_at, Date.parse('2013-07-22'))
+    feed.update_attribute(:fetched_at, DateTime.parse('2013-07-22 22:30'))
 
     feed.fetch
 
     assert_equal 1, feed.new_entries.length
     assert_equal 'Going brompton', feed.new_entries.first.title
     assert_equal 'Sous les briques, le soleil', feed.title
+  end
+
+  test 'get empty array if no new entry' do
+    feed = feeds(:one)
+    feed.update_attribute(:fetched_at, DateTime.now)
+
+    feed.fetch
+
+    assert_equal 0, feed.new_entries.length
   end
 end
